398109ea58f8d00405f6ac99c91873db69259e79
leminhquy <leminhquytp@gmail.com>
2024-04-16 08:53:33 +0700

    ss

---
+++ Assignment1/utilities/meta_info.py
@@ -0,0 +1,159 @@
+import hashlib
+import time
+import os
+
+class Bencode:
+    def __init__(self):
+        pass
+    @staticmethod
+    def decode_string(bencoded_value) -> tuple:
+        first_colon_index = bencoded_value.find(b":")
+        if first_colon_index == -1:
+            raise ValueError("Invalid encoded value")
+        length = int(bencoded_value[:first_colon_index])
+        start_index = first_colon_index + 1
+        end_index = start_index + length
+        if end_index > len(bencoded_value):
+            raise ValueError("Invalid encoded value")
+        value = bencoded_value[start_index:end_index]
+        length = first_colon_index + length + 1
+        return value, length
+    @staticmethod
+    def decode_integer(bencoded_value):
+        end_index = bencoded_value.find(b"e")
+        if end_index == -1:
+            raise ValueError("Invalid encoded value")
+        value = int(bencoded_value[1:end_index])
+        length = end_index + 1
+        return value, length
+    @staticmethod
+    def decode_list(bencoded_value):
+        result = []
+        i = 1
+        while i < len(bencoded_value):
+            if chr(bencoded_value[i]) == "e":
+                break
+            if chr(bencoded_value[i]).isdigit():
+                value, length = Bencode.decode_string(bencoded_value[i:])
+            elif chr(bencoded_value[i]) == "i":
+                value, length = Bencode.decode_integer(bencoded_value[i:])
+            elif chr(bencoded_value[i]) == "l":
+                value, length = Bencode.decode_list(bencoded_value[i:])
+            elif chr(bencoded_value[i]) == "d":
+                value, length = Bencode.decode_dictionary(bencoded_value[i:])
+            i += length
+            result.append(value)
+        return result, i + 1
+    @staticmethod
+    def decode_dictionary(bencoded_value):
+        result = {}
+        i = 1
+        while i < len(bencoded_value):
+            if chr(bencoded_value[i]) == "e":
+                break
+            key, key_length = Bencode.decode_string(bencoded_value[i:])
+            i += key_length
+            if chr(bencoded_value[i]).isdigit():
+                value, length = Bencode.decode_string(bencoded_value[i:])
+            elif chr(bencoded_value[i]) == "i":
+                value, length = Bencode.decode_integer(bencoded_value[i:])
+            elif chr(bencoded_value[i]) == "l":
+                value, length = Bencode.decode_list(bencoded_value[i:])
+            elif chr(bencoded_value[i]) == "d":
+                value, length = Bencode.decode_dictionary(bencoded_value[i:])
+            i += length
+            result[key.decode()] = value
+        result = dict(sorted(result.items()))
+        return result, i + 1
+    @staticmethod
+    def decode(bencoded_value):
+        if chr(bencoded_value[0]).isdigit():
+            value, _ = Bencode.decode_string(bencoded_value)
+            return value
+        elif chr(bencoded_value[0]) == "i":
+            value, _ = Bencode.decode_integer(bencoded_value)
+            return value
+        elif chr(bencoded_value[0]) == "l":
+            value, _ = Bencode.decode_list(bencoded_value)
+            return value
+        elif chr(bencoded_value[0]) == "d":
+            value, _ = Bencode.decode_dictionary(bencoded_value)
+            return value
+        else:
+            raise NotImplementedError("Unknown bencode type")
+    @staticmethod
+    def encode(value):
+        if isinstance(value, str):
+            return f"{len(value)}:{value}".encode()
+        elif isinstance(value, bytes):
+            return f"{len(value)}:".encode() + value
+        elif isinstance(value, int):
+            return f"i{value}e".encode()
+        elif isinstance(value, list):
+            result = b"l"
+            for item in value:
+                result += Bencode.encode(item)
+            result += b"e"
+            return result
+        elif isinstance(value, dict):
+            result = b"d"
+            for key, item in value.items():
+                result += Bencode.encode(key)
+                result += Bencode.encode(item)
+            result += b"e"
+            return result
+        else:
+            raise NotImplementedError(f"Unknown type {type(value)}")
+        
+class MetaInfo:
+    def __init__(self, data):
+        self.announce = data["announce"]
+        self.info = data["info"]
+        self.length = self.info["length"]  # single file case
+        self.files = None  # multi files case
+        self.piece_length = self.info["piece length"]
+        self.pieces = self.info["pieces"]
+        self.info_hash = hashlib.sha1(Bencode.encode(self.info)).digest()   #the actual hash value of the data and returns it as a bytes object.
+        self.info_hash_hex = self.info_hash.hex()   #from bytes to hexadecimal
+        
+    def get_piece_hashes(self):
+        return [self.pieces[i : i + 20].hex() for i in range(0, len(self.pieces), 20)]  #20 bytes/piece
+    
+def create_torrent_file(file_path, tracker_url, piece_length=262144):  # Default piece length is 256KB
+    # Get file info
+    file_size = os.path.getsize(file_path)
+    file_name = os.path.basename(file_path)
+
+    # Read the file in pieces and calculate the hash for each piece
+    pieces = b''
+    with open(file_path, 'rb') as f:
+        while True:
+            piece = f.read(piece_length)
+            if not piece:
+                break
+            pieces += hashlib.sha1(piece).digest()
+
+    # Create the torrent file content
+    torrent = {
+        'announce': tracker_url,
+        'creation date': int(time.time()),
+        'info': {
+            'name': file_name,
+            'piece length': piece_length,
+            'length': file_size,
+            'pieces': pieces
+        }
+    }
+
+    # Bencode the torrent file content
+    torrent_bencoded = Bencode.encode(torrent)
+
+    # Write the torrent file
+    torrent_file_name = file_name.split('.')[0] + '.torrent'
+    with open(torrent_file_name, 'wb') as f:
+        f.write(torrent_bencoded)
+
+    print(f'Torrent file created: {torrent_file_name}')
+    
+if __name__ == "__main__":
+    create_torrent_file("swe.pdf", f"http://192.168.2.87:55555/announce")
