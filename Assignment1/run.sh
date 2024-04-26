a0fe1f5830e1ba4856d29347f4135d65c7ebcddf
Quy Le <leminhquytp@gmail.com>
2024-04-26 21:16:29 +0700

    Running scripts

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
-
-if [-d "result"]; then
+export PYTHONPATH=./
+if [ -d "result" ]; then
     rm -rf "result"
 fi
-
 mkdir "result"
-python main.py download -o result data.torrent
+
+commands=("info" "download" "download_rarest_first")
+
+# Print the list of commands
+echo "Available commands:"
+for command in "${commands[@]}"; do
+    echo "  $command"
+done
+
+read -p "Enter command: " command
+
+python server.py
+python client.py
+
+if [ "$command" == "info" ]; then
+    read -p "Enter torrent file name: " torrent_file_name
+    python main.py info "$torrent_file_name"
+elif [ "$command" == "download" ]; then
+    read -p "Enter output directory: " output_directory
+    read -p "Enter torrent file name: " torrent_file_name
+    python3 main.py download -o "$output_directory" "$torrent_file_name"
+elif [ "$command" == "download_rarest_first" ]; then
+    read -p "Enter output directory: " output_directory
+    read -p "Enter torrent file name: " torrent_file_name    
+    python download_using_trategy.py download -o "$output_directory" "$torrent_file_name"
+else
+    echo "Unknown command $command"
+fi
